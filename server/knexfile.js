06642e8ce44e63c948a1f0160390663bd087b2e5
Christopher White <Maverick@Christophers-MacBook-Air.local>
2017-10-01 22:29:11 -0600

    heroku mods

--- server/knexfile.js
+++ server/knexfile.js
@@ -6,4 +6,8 @@ module.exports = {
     client: 'pg',
     connection: 'postgres://localhost/test-database'
   },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+  },
 };
